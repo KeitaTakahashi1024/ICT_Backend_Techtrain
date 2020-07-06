5bcebb42bb1615fd28967ed94dda819228a63d97
KeitaTakahashi1024 <b1017172@fun.ac.jp>
2020-07-06 17:48:22 +0900

    [add]localhostにユーザーのcreate/get/update用のページを作成した

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import(
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/user/create", createUser)
+	http.HandleFunc("/user/get", getUser)
+	http.HandleFunc("/user/update", updateUser)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, from Docker container!")
+
+
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to user/create, from Docker container!")
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to user/get, from Docker container!")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to user/update, from Docker container!")
 }
